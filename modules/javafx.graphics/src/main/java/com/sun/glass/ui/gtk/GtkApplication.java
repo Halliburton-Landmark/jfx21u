3e5ae57e8b0f47f21b4e4b9bd7cdcffbc1c689b6
Oleksandr Zakusilo <Oleksandr.Zakusilo@halliburton.com>
2019-04-01 11:34:46 +0300

    Pass scaling factor from SWT to FX # Conflicts: #	modules/javafx.graphics/src/main/java/com/sun/glass/ui/gtk/GtkApplication.java

--- modules/javafx.graphics/src/main/java/com/sun/glass/ui/gtk/GtkApplication.java
+++ modules/javafx.graphics/src/main/java/com/sun/glass/ui/gtk/GtkApplication.java
@@ -234,8 +234,23 @@ final class GtkApplication extends Application implements
         @SuppressWarnings("removal")
         final boolean disableGrab = AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> Boolean.getBoolean("sun.awt.disablegrab") ||
                Boolean.getBoolean("glass.disableGrab"));
+        
+	int windowScale = getWindowScale();
 
-        _init(eventProc, disableGrab);
+        _init(eventProc, disableGrab, windowScale);
+    }
+
+    private int getWindowScale() {
+        String type = AccessController.doPrivileged((PrivilegedAction<String>) () -> System.getProperty("com.sun.javafx.application.type", ""));
+    	if ("FXCanvas".equals(type)) {
+            PlatformLogger logger = Logging.getJavaFXLogger();
+            final int scale = AccessController.doPrivileged((PrivilegedAction<Integer>) () -> Integer.getInteger("org.eclipse.swt.internal.deviceZoom", 100));
+            logger.warning("scale=" + scale);
+            System.out.println("====== SCALE " + scale);
+            return scale / 100;
+    	}
+        System.out.println("====== SCALE FAILURE ===== ");
+    	return 1;
     }
 
     @Override
@@ -282,7 +297,7 @@ final class GtkApplication extends Application implements
 
     private native void _terminateLoop();
 
-    private native void _init(long eventProc, boolean disableGrab);
+    private native void _init(long eventProc, boolean disableGrab, int windowScale);
 
     private native void _runLoop(Runnable launchable, boolean noErrorTrap);
 
